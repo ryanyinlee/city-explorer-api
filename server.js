d9832c277b222c97e2062e1c81d2a8cf3d1de096
Ryan Lee <ryanyinlee@protonmail.com>
2021-12-08 11:33:02 -0800

    think it's workin?

--- server.js
+++ server.js
@@ -7,34 +7,53 @@ const app = express();
 const cors = require('cors');
 
 const PORT = process.env.PORT // DO NOT MOVE THIS
-const requestWeather = require('./data/weather.json');
+const weatherData = require('./data/weather.json');
+const axios = require('axios');
+// formerly requestWeather
 // const { response } = require('express');
 
 app.use(cors());
 
 app.get('/data/weather.json', handleGetWeather)
 // error stuff
-app.get('/*', errorHandler);
+//app.get('/*', errorHandler);
 
 function errorHandler(request, response) {
-    response.status(500).send("thing broke");
+    response.status(500).send("thing broke :C");
 }
 
 
-
+let newWeatherItem = [];
 function handleGetWeather(request, response) {
-    console.log(request);
-    console.log(request.query);
-    const findlat = request.query.lat;
-    const findlon = request.query.lon;
-    let newWeatherItem = list.map(weather => new Forecast(weather));
-    response.status(200).send(requestWeather);
-    response.status(200).send(newWeatherItem);
+    // console.log(request);
+    // console.log(request.query);
+    // let findlat = request.query.lat;
+    // let findlon = request.query.lon;
+    // let city_name = request.query.city_name;
+    // // https://api.weatherbit.io/v2.0/current?lat=${req.query.lat}&lon=${req.query.lon}&key=WEATHER_API_KEY
+    const url = `https://api.weatherbit.io/v2.0/current?lat=${request.query.lat}&lon=${request.query.lon}&key=${process.env.WEATHER_API_KEY}`
+    // let cityMatch = weatherData.find(city => city.city_name.toLowerCase() === city_name.toLowerCase());
+    // if (cityMatch) {
+    //     let weatherDescription = cityMatch.data.map(day => new Forecast(day));
+    //     response.status(200).send(weatherDescription);
+    // } else {
+    //     response.status(400).send('no data on city');
+    // }
+    // newWeatherItem.map(weather => new Forecast(weather));
+    // response.status(200).send(weatherData);
+    // response.status(200).send(newWeatherItem);
+    // response.status(200).send(findlat);
+    // response.status(200).send(findlon);
+    // response.status(200).send(city_name);
+
+    axios.get(url)
+        .then(results=> console.log(results))
+        .catch(error => console.log(error));
 }
 
 class Forecast {
     constructor(obj) {
-        this.datetime = obj.datetime,
+        this.date = obj.datetime,
         this.description = obj.description
     }
 }
