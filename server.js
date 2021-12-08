831afb7def74600cef0414aa3d7c017199ffb479
Ryan Lee <ryanyinlee@protonmail.com>
2021-12-08 03:11:46 -0800

    error handling

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
 'use strict'
 //import axios from 'axios'
-const PORT = process.env.PORT
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 
-
+const PORT = process.env.PORT // DO NOT MOVE THIS
 const requestWeather = require('./data/weather.json');
 // const { response } = require('express');
 
-
-
 app.use(cors());
 
-
-
 app.get('/data/weather.json', handleGetWeather)
+// error stuff
+app.get('/*', errorHandler);
 
-
-function handleGetWeather(request, response) {
-    response.send(requestWeather);
+function errorHandler(request, response) {
+    response.status(500).send("thing broke");
 }
 
 
-// function handleWeather(request, response) {
-//     response.status(200).send(requestWeather);
-//     response.send('weather info incoming');
-// }
 
+function handleGetWeather(request, response) {
+    console.log(request);
+    console.log(request.query);
+    const findlat = request.query.lat;
+    const findlon = request.query.lon;
+    let newWeatherItem = list.map(weather => new Forecast(weather));
+    response.status(200).send(requestWeather);
+    response.status(200).send(newWeatherItem);
+}
 
+class Forecast {
+    constructor(obj) {
+        this.datetime = obj.datetime,
+        this.description = obj.description
+    }
+}
 
 
 app.listen(PORT, () => console.log('server is listening on port: ', PORT));
