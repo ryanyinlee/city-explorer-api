b807ac2e945f4e98d37ee8defbe3d3b77ca6d166
Ryan Lee <ryanyinlee@protonmail.com>
2021-12-10 01:23:36 -0800

    need to fix

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors());
 const PORT = process.env.PORT // DO NOT MOVE THIS FROM LINE 9
 
 const axios = require('axios');
+const { reset } = require('nodemon');
 
 app.get('/test', ((request, response) => response.send('server workin'))); // test only
 app.get('/data/weather.json');
@@ -29,11 +30,12 @@ function errorHandler(req, res) {
     res.status(404).send("thing not there");
 }
 
-// let newWeatherItem = [];
+let newWeatherItem = [];
 
 
-function handleGetWeather(req, res) {
-    
+
+async function handleGetWeather(req, res) {
+
     //let city_name = req.query.city_name; // No longer use city. Need to use lat/lon
 
     const url = `https://api.weatherbit.io/v2.0/current?lat=${req.query.lat}&lon=${req.query.lon}&key=${process.env.WEATHER_API_KEY}`
@@ -46,13 +48,43 @@ function handleGetWeather(req, res) {
     //res.status(200).send(newWeatherItem);
     res.status(200).send(findlat);
     res.status(200).send(findlon);
-    
+
     axios.get(url)
-        .then(results=> console.log(results))
+        .then(results => console.log(results))
         .catch(error => console.log(error));
 
 
 }
+
+async function handleGetMovies(req, res) {
+
+    const { city_name } = req.query;
+
+
+    // put in a try catch (error)
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.MOVIE_API_KEY}
+&language=en-US&wuery=${city_name}$page=1&include_adult=false`
+
+    const movieResponse = await axios.get(url);
+    const cleanedMovies = movieResponse.data.map(movie => new Movies(movie));
+    res.send(cleanedMovies.data);
+
+}
+// obj is the stuff in the object
+class Movies {
+    constructor(obj) {
+        this.title = obj.title,
+        this.overview = obj.overview,
+        this.average_votes = obj.average_votes,
+        this.total_votes = obj.total_votes,
+        this.image_url = obj.image_url,
+        this.popularity = obj.popularity,
+        this.released_on = obj.released_on
+    }
+}
+
+
+
 
 class Forecast {
     constructor(obj) {
