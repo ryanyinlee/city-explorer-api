432aebb6b87eb2211af2b53a43180455d32e1938
Ryan Lee <ryanyinlee@protonmail.com>
2021-12-12 21:39:12 -0800

    remodularizationfactorizing

---
+++ routeHandlers/weather.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const axios = require('axios');
+
+function handleGetWeather(req, res) {
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&key=${process.env.WEATHER_API_KEY}&units=I`;  
+    console.log(req.query)
+    axios.get(url)
+      .then(results => {        
+        let weatherDescriptions = results.data.data.map(day => new Forecast(day));
+        res.status(200).send(weatherDescriptions);
+      })
+      .catch (error => {
+        console.error(error.message);
+        res.status(500).send('server error');
+      });
+
+      
+}
+
+
+class Forecast {
+    constructor(obj) {
+        this.date = obj.datetime,
+        this.description = `High temp: ${obj.max_temp}, Low Temp: ${obj.low_temp}, With: ${obj.weather.description}`
+    }
+}
+
+module.exports = handleGetWeather;
